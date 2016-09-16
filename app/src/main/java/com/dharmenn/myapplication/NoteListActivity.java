86b90fb27969be9cafe6a09038c93d9d6e83ff92
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-09-16 11:51:20 +0530

    implementing methods to display all notes in  list view

--- app/src/main/java/com/dharmenn/myapplication/NoteListActivity.java
+++ app/src/main/java/com/dharmenn/myapplication/NoteListActivity.java
@@ -3,21 +3,35 @@ package com.dharmenn.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class NoteListActivity extends Activity {
+
+    private DataBaseHelper dataBaseHelper = new DataBaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_list);
+        populateNotesList();
 
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-//        textView.setText(message);
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
-        layout.addView(textView);
+    }
 
+    private void populateNotesList() {
+        ListView textView = (ListView) findViewById(R.id.note_list_view);
+        List<String> allNotes = dataBaseHelper.getAllNotes();
+        Log.d("myTag", allNotes.toString());
+        ArrayAdapter<String> notesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, allNotes);
+        textView.setAdapter(notesAdapter);
+    }
+
+    public void addNewNote(View view) {
+        Intent intent = new Intent(this, EditNoteActivity.class);
+        startActivity(intent);
     }
 }
